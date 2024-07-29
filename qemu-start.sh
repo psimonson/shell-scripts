9e1f96a47cb8f8ec2c88572094d9f2fd790310fa
Philip R. Simonson <psimonson1988@gmail.com>
2024-07-29 01:18:35 -0400

    Fixed script

--- qemu-start.sh
+++ qemu-start.sh
@@ -5,16 +5,16 @@ if [ $# -eq 2 ]
 then
 	if [ "$1" = "-hda" ]
 	then
-		sudo sh -c "qemu-system-x86_64 -hda $2 -m 4096 -cpu host -enable-kvm &"
+		sudo -E qemu-system-x86_64 -hda $2 -m 8192
 	elif [ "$1" = "-cdrom" ]
 	then
-		sudo sh -c "qemu-system-x86_64 -cdrom $2 -m 4096 -cpu host -enable-kvm &"
+		sudo -E qemu-system-x86_64 -cdrom $2 -m 8192
 	else
-		sudo sh -c "qemu-system-x86_64 -hda $1 -cdrom $2 -m 4096 -cpu host -enable-kvm &"
+		sudo -E qemu-system-x86_64 -hda $1 -cdrom $2 -m 8192
 	fi
 elif [ $# -eq 3 ]
 then
-	sudo sh -c "qemu-system-x86_64 -hda $1 -cdrom $2 -boot $3 -m 4096 -cpu host -enable-kvm &"
+	sudo -E qemu-system-x86_64 -hda $1 -cdrom $2 -boot $3 -m 8192
 else
-	echo "Usage: $0 [harkdisk-image] <cdrom>"
+	echo "Usage: $0 [harkdisk-image] [cdrom] [boot]"
 fi
