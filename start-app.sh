f6797f97ce626f0b1fe6360f50a5769efec0f9f8
Philip R. Simonson <psimonson1988@gmail.com>
2024-09-06 14:54:36 -0400

    Fixed for my new setup

--- start-app.sh
+++ start-app.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 exec_steamapp() {
-	local steamapps_path="${HOME}/.steam/steam/steamapps"
+	local steamapps_path="/data-hdd/SteamLibrary/steamapps"
 	local compatdata_path="${steamapps_path}/compatdata/$1"
 	local app_path="$2"
 
-	if [[ -x "$app_path" && -x "$compatdata_path" ]]
+	if [[ -x "$compatdata_path" ]]
 	then
 		export STEAM_COMPAT_CLIENT_INSTALL_PATH="$steamapps_path"
 		export STEAM_COMPAT_DATA_PATH="$compatdata_path"
-		"${steamapps_path}/common/Proton 8.0/proton" run "${app_path}"
+		"${steamapps_path}/common/Proton - Experimental/proton" run "${app_path}"
 	else
 		echo "Game or app not found."
 	fi
